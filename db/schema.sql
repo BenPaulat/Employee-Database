90441dc904c70ed2ebbb0d208fc7da403f392740
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-11 19:16:23 -0600

    added employee table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,11 @@ CREATE TABLE role (
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) UNIQUE NOT NULL,
+    last_name VARCHAR(30) UNIQUE NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT UNSIGNED NOT NULL
+);
