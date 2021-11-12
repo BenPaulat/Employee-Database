6fa587c3940de836e8ca6a1480104c7a5af87fc5
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-11 19:19:11 -0600

    added Db and consoleTable to server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 // bring in inq, db, console.table
 // function to load main prompts (inquirer)
 // choices = [of objects] (name, value) .then (Switch statement = case (value from prev) function view departments (call sql query))
+
+const Db = require('./db/index');
+const consoleTable = require('console.table');
