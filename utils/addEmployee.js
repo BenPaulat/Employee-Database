625cfd318d90d5e5de7090c93470658c8f7f1b4c
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-14 21:01:09 -0600

    updated questions for addEmployee

--- utils/addEmployee.js
+++ utils/addEmployee.js
@@ -3,11 +3,27 @@ const consoleTable = require('console.table');
 const inquire = require('inquirer');
 
 function addEmployee(questions) {
-    inquire.prompt({
+    inquire.prompt([{
         type: 'input',
-        name: 'name',
-        message: "What is the employee's name?"
-    }).then(answer => {
+        name: 'first_name',
+        message: "What is the employee's first name?"
+    },
+    {
+        type: 'input',
+        name: 'last_name',
+        message: "What is the employee's last name?"
+    },
+    {
+        type: 'input',
+        name: 'role',
+        message: "What is the employee's role?"
+    },
+    {
+        type: 'input',
+        name: 'manager',
+        message: "Who is the employee's manager?"
+    }
+]).then(answer => {
         Db.addEmployee(answer);  
     }).then(() => questions());
 };
