dd56a3af56441c50f965fcb7a44a5de8def25901
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-14 22:01:26 -0600

    fixed issue in schema where names needed to be unique

--- db/schema.sql
+++ db/schema.sql
@@ -18,8 +18,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) UNIQUE NOT NULL,
-    last_name VARCHAR(30) UNIQUE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     manager_id INT UNSIGNED
 );
