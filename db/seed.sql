02ea7257b72f3b940e979c64f9c15f990691a571
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-14 20:35:53 -0600

    added seed data and fixed issue with find Roles

--- db/seed.sql
+++ db/seed.sql
@@ -3,9 +3,14 @@ USE employee;
 INSERT INTO department
 (name)
 VALUES
-('Engineering'), ('Management'), ('Custodian');
+('Management'), ('Shipping'), ('Accounting'), ('Medical'), ('Engineering'), ('Janitorial');
 
 INSERT INTO role
-(title, salary, department_id)
+(title, salary, department_id);
 VALUES
-('Manager', 60000, 12)
+('CEO/Manager', 150000, 1), ('Delivery Boy', 40000, 2), ('Delivery Captain', 45000, 2), ('Assistant Delivery Unit', 40000, 2), ('Accountant', 65000, 3), ('Staff Doctor', 10000, 4), ('Intern', 40000, 5), ('Janitor', 35000, 6);
+
+INSERT INTO empmloyee
+(first_name, last_name, role_id, manager_id);
+VALUES
+(Philip, Fry, 2, 10), (Turanga, Leela, 2, 10), (Bender, Rodriguez, 2, 10), (Amy, Wong, 5, 10), (Hermes, Conrad, 3, 10), (Hubert, Farnsworth, 1, NULL), (John, Zoidberg, 4, 10), (Scruffy, Scruffington, 6, 10);
--- utils/viewRoles.js
+++ utils/viewRoles.js
@@ -3,7 +3,7 @@ const consoleTable = require('console.table');
 const inquire = require('inquirer');
 
 function viewRoles(questions) {
-    Db.findAllDepartment().then(([data]) => {
+    Db.findAllRoles().then(([data]) => {
         console.log(data)
     }).then(() => questions())
 }
